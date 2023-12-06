0edd42d33bed097878b6e3e7e09cdab8f5c8ea53
8-bitMeer <144816003+8-bitMeer@users.noreply.github.com>
2023-12-06 15:54:43 +0000

    project

--- project.py
+++ project.py
@@ -6,3 +6,7 @@ print('please select an Artist')
 print(select)
 print('please select a song')
 ['select 1 for Gods country, 2 for Old friend, 3 for Take me away']
+
+About:#My program is about users selecting music and listening to music. This program is made to play music by selecting a certain genre and a specific song by an artist. There can be a few setbacks when it comes to playing music, such as no internet or signal. it can have ups and downs, but ultimately, it can provide users the sweet sounds
+
+Contributors: #special thanks to Elderbrook and Spotify/YouTube music.
