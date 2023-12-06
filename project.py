4225c604ad80e247b8fbafc6ef381d147cb7cad7
8-bitMeer <144816003+8-bitMeer@users.noreply.github.com>
2023-12-06 15:50:48 +0000

    Project

---
+++ project.py
@@ -0,0 +1,8 @@
+Artists=["blake shelton", "Elderbrook", "Ayron jones"]
+Songs=["Gods Country", "Old friend", "Take Me away"]
+print(select)
+print('please select an Artist')
+['select 1 forBlakeshelton, 2 forElderbrook, 3 forAyronJones']
+print(select)
+print('please select a song')
+['select 1 for Gods country, 2 for Old friend, 3 for Take me away']
